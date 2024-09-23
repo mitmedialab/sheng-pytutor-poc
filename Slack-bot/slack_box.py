603aea63fb03bdf6a2c4b3735c4211969fa38594
Sheng Huang <103220922+shengh318@users.noreply.github.com>
2024-09-23 15:36:51 -0400

    start of chat gpt box

--- Slack-bot/slack_box.py
+++ Slack-bot/slack_box.py
@@ -6,5 +6,19 @@ from dotenv import load_dotenv
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
+PY_TUTOR_CHANNEL_ID: str = "C07N96PA0F8"
+
 client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
-client.chat_postMessage(channel='#pytutor-slack-bot-test', text="Hello World!")
+post_message_data = client.chat_postMessage(
+    channel=PY_TUTOR_CHANNEL_ID, text="I like apples.")
+print(post_message_data)
+
+
+def get_channel_history(channel_id: str) -> list:
+    response = client.conversations_history(
+        channel=channel_id, inclusive=True)
+    messages = response['messages']
+    return messages
+
+
+# print(get_channel_history(PY_TUTOR_CHANNEL_ID))
---
+++ chat_box.py
@@ -0,0 +1,47 @@
+import os
+from typing import Union
+from pathlib import Path
+from dotenv import load_dotenv
+import requests
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+
+class chat_box:
+    def __init__(self):
+        self.gpt_token = os.environ['GPT_TOKEN']
+        self.api_url = "https://api.openai.com/v1/chat/completions"
+
+    def send_message(self, message: str) -> Union[dict, str]:
+        """
+        Send a message to the GPT-4o model and get a response.
+        """
+
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {self.gpt_token}'
+        }
+        data = {
+            'model': 'gpt-4o',
+
+            'messages': [
+                {
+                    "role": "system",
+                    "content": "You are a helpful assistant."
+                },
+                {
+                    'role': 'user',
+                    'content': message
+                }
+            ]
+        }
+
+        response = requests.post(
+            self.api_url,
+            json=data,
+            headers=headers,
+            timeout=10
+        )
+
+        return response.json()['choices'][0]['message']['content'] if response.status_code == 200 else response.text
