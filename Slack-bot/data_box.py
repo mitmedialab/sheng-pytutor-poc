afcd25900512085218ff72f2353fcbba24875934
Sheng Huang <103220922+shengh318@users.noreply.github.com>
2024-09-28 11:37:06 -0400

    working local database connection and slack bot replying in thread / starting new convo

--- Slack-bot/data_box.py
+++ Slack-bot/data_box.py
@@ -1,13 +1,74 @@
-
+import asyncpg
 from typing import Optional
 
 
 class Data_Box:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, dbname: str, user: str, password: str, host: str, port: str) -> None:
+        self.dbname = dbname
+        self.user = user
+        self.password = password
+        self.host = host
+        self.port = port
+        self.connection = None
 
-    def get_thread_ts(self, student_id: str, workspace_id: str) -> Optional[str]:
-        pass
+    async def connect(self) -> None:
+        """
+        Connect to the database.
+        """
+        self.connection = await asyncpg.connect(
+            user=self.user,
+            password=self.password,
+            database=self.dbname,
+            host=self.host,
+            port=self.port
+        )
 
-    def add_thread_ts_to_db(self, student_id: str, workspace_id: str, thread_ts: str) -> None:
-        pass
+        if self.connection is not None:
+            print("Connected to the database.")
+        else:
+            print("Failed to connect to the database.")
+
+    async def get_thread_ts(self, student_id: str, workspace_id: str) -> Optional[str]:
+        """
+        Get the thread_ts from the database using {student_id}_{workspace_id} as the key.
+
+        Args:
+            student_id (str): The student's ID.
+            workspace_id (str): The workspace's ID.
+
+        Returns:
+            Optional[str]: The thread_ts if it exists, otherwise None.
+
+        """
+        db_key: str = f"{student_id}_{workspace_id}"
+        query: str = "SELECT thread_ts FROM students WHERE student_id = $1;"
+        thread_ts = await self.connection.fetchval(query, db_key)
+        return thread_ts
+
+    async def add_thread_ts_to_db(self, student_id: str, workspace_id: str, thread_ts: str) -> None:
+        """
+        Add a thread_ts to the database using {student_id}_{workspace_id} as the key.
+
+        Args:
+            student_id (str): The student's ID.
+            workspace_id (str): The workspace's ID.
+            thread_ts (str): The thread_ts to add to the database.
+
+        Returns:
+            None
+
+        """
+        db_key: str = f"{student_id}_{workspace_id}"
+        query: str = "INSERT INTO students (student_id, thread_ts) VALUES ($1, $2) ON CONFLICT (student_id) DO UPDATE SET thread_ts = EXCLUDED.thread_ts;"
+        await self.connection.execute(query, db_key, thread_ts)
+
+    async def close(self) -> None:
+        """
+        Close the connection to the database.
+        Should be called when the program is done using the database.
+        """
+        if self.connection:
+            await self.connection.close()
+            print("Connection to the database closed.")
+        else:
+            print("No connection to close.")
--- Slack-bot/slack_box.py
+++ Slack-bot/slack_box.py
@@ -1,5 +1,5 @@
-from cgi import test
 import os
+import asyncio
 from typing import Optional
 from pathlib import Path
 import slack
@@ -16,9 +16,13 @@ class Slack_Box:
         self.STUDENT_TOKEN: Optional[str] = os.getenv('STUDENT_SLACK_TOKEN')
         self.CHANNEL_ID: Optional[str] = os.getenv('PY_TUTOR_CHANNEL_ID')
 
-        assert self.TUTOR_TOKEN is not None, "SLACK_TOKEN is not set in .env"
-        assert self.CHANNEL_ID is not None, "PY_TUTOR_CHANNEL_ID is not set in .env"
-        assert self.STUDENT_TOKEN is not None, "STUDENT_SLACK_TOKEN is not set in .env"
+        self.DB_Name: Optional[str] = os.getenv('DB_NAME')
+        self.DB_User: Optional[str] = os.getenv('DB_USER')
+        self.DB_Password: Optional[str] = os.getenv('DB_PASSWORD')
+        self.DB_Host: Optional[str] = os.getenv('DB_HOST')
+        self.DB_Port: Optional[str] = os.getenv('DB_PORT')
+
+        self.check_init()
 
         self.tutor_client: slack.WebClient = slack.WebClient(
             token=self.TUTOR_TOKEN)
@@ -26,9 +30,31 @@ class Slack_Box:
         self.student_client: slack.WebClient = slack.WebClient(
             token=self.STUDENT_TOKEN)
 
-        self.data_box: Data_Box = Data_Box()
+        self.data_box: Data_Box = Data_Box(
+            dbname=self.DB_Name,
+            user=self.DB_User,
+            password=self.DB_Password,
+            host=self.DB_Host,
+            port=self.DB_Port
+        )
 
-    def post_message(self, student_id: str, workspace_id: str, message: str, role: str = "Tutor") -> None:
+    def check_init(self) -> None:
+        """
+        Check if the Slack_Box was initialized correctly.
+
+        Returns:
+            None
+        """
+        assert self.TUTOR_TOKEN is not None, "SLACK_TOKEN is not set in .env"
+        assert self.CHANNEL_ID is not None, "PY_TUTOR_CHANNEL_ID is not set in .env"
+        assert self.STUDENT_TOKEN is not None, "STUDENT_SLACK_TOKEN is not set in .env"
+        assert self.DB_Name is not None, "DB_NAME is not set in .env"
+        assert self.DB_User is not None, "DB_USER is not set in .env"
+        assert self.DB_Password is not None, "DB_PASSWORD is not set in .env"
+        assert self.DB_Host is not None, "DB_HOST is not set in .env"
+        assert self.DB_Port is not None, "DB_PORT is not set in .env"
+
+    async def post_message(self, student_id: str, workspace_id: str, message: str, role: str = "Tutor") -> None:
         """
         Post a message to the Slack channel. If the student has a thread_ts, post the message in the thread.
         Otherwise create a new thread and post the message in the thread.
@@ -43,7 +69,7 @@ class Slack_Box:
             None
         """
 
-        thread_ts: Optional[str] = self.data_box.get_thread_ts(
+        thread_ts: Optional[str] = await self.data_box.get_thread_ts(
             student_id=student_id, workspace_id=workspace_id)
 
         if role == "Tutor":
@@ -58,10 +84,23 @@ class Slack_Box:
             response = client.chat_postMessage(
                 channel=self.CHANNEL_ID, text=message)
             thread_ts = response['ts']
-            self.data_box.add_thread_ts_to_db(
+            await self.data_box.add_thread_ts_to_db(
                 student_id=student_id, workspace_id=workspace_id, thread_ts=thread_ts)
 
         if not response['ok']:
             print(response['error'])
         else:
             print("Message sent successfully")
+
+
+if __name__ == "__main__":
+    async def main():
+        """
+        Test the Slack_Box class.
+        """
+        test_box = Slack_Box()
+        await test_box.data_box.connect()
+        await test_box.post_message("123", "456", "DB Test")
+        await test_box.data_box.close()
+
+    asyncio.run(main())
